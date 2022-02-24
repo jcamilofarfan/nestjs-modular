653339537185e8b18b3fb85e017f96efbb28a65a
jcamilofarfan <farfan1976@hotmail.com>
2022-02-24 15:59:20 -0500

    useFactory

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,10 @@ import { Injectable, Inject } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  constructor(@Inject('API_KEY') private readonly apiKey: string) {}
+  constructor(
+    @Inject('API_KEY') private readonly apiKey: string,
+    @Inject('TASKS') private tasks: any[],
+  ) {}
   getHello(): string {
     return `Hello World! ${this.apiKey}`;
   }
