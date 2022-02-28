02db32702ad834f5f596eabe484d55718cbf619a
jcamilofarfan <farfan1976@hotmail.com>
2022-02-28 12:57:03 -0500

    Tipado en config

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Inject } from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
+
+import config from './config';
 
 @Injectable()
 export class AppService {
+  constructor(
+    @Inject(config.KEY) private configService: ConfigType<typeof config>,
+  ) {}
   getHello(): string {
+    const apiKey = this.configService.apiKey;
+    const name = this.configService.database.name;
+    console.log(apiKey);
+    console.log(name);
     return `Hello World!`;
   }
 }
