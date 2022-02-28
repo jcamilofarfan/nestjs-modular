08758c8139e0bb4c874c2d5ada316b714fed02da
jcamilofarfan <farfan1976@hotmail.com>
2022-02-28 08:42:11 -0500

    globalmodule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,19 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
-
-const API_KEY = '123456789';
-const API_KEY_PROD = 'prod-123456789';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [UsersModule, ProductsModule],
+  imports: [UsersModule, ProductsModule, DatabaseModule],
   controllers: [AppController],
-  providers: [
-    AppService,
-    {
-      provide: 'API_KEY',
-      useValue: process.env.NODE_ENV === 'prod' ? API_KEY_PROD : API_KEY,
-    },
-  ],
+  providers: [AppService],
 })
 export class AppModule {}
