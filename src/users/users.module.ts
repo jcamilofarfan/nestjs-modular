2bc1f672f19feba078c1ee720d450f823c1d061c
jcamilofarfan <farfan1976@hotmail.com>
2022-03-08 08:36:56 -0500

    creando entidades

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,14 +1,20 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { CustomerController } from './controllers/customers.controller';
 import { CustomersService } from './services/customers.service';
+import { Customer } from './entities/customer.entity';
+
 import { UsersController } from './controllers/users.controller';
 import { UsersService } from './services/users.service';
+import { User } from './entities/user.entity';
+
+import { Order } from './entities/order.entity';
 
 import { ProductsModule } from '../products/products.module';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [ProductsModule, TypeOrmModule.forFeature([Customer, User, Order])],
   controllers: [CustomerController, UsersController],
   providers: [CustomersService, UsersService],
 })
